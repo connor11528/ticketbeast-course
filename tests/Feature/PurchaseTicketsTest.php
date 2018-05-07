72edf9681d1fd27eb0121f112cb14aeeda8e526f
Connor Leech <connor@stitchlabs.com>
2018-05-07 13:53:39 -0700

    2.7 validation testing

--- tests/Feature/PurchaseTicketsTest.php
+++ tests/Feature/PurchaseTicketsTest.php
@@ -19,6 +19,13 @@ class PurchaseTicketsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->paymentGateway = new FakePaymentGateway;
+        $this->app->instance(PaymentGateway::class, $this->paymentGateway);
+    }
+
     /** @test */
     function customer_can_purchase_concert_tickets()
     {
@@ -26,8 +33,6 @@ class PurchaseTicketsTest extends TestCase
         // $this->withExceptionHandling();
 
         // Arrange
-        $paymentGateway = new FakePaymentGateway;
-        $this->app->instance(PaymentGateway::class, $paymentGateway);
         $concert = factory(Concert::class)->create([
             'ticket_price' => 3250
         ]);
@@ -36,16 +41,30 @@ class PurchaseTicketsTest extends TestCase
         $this->json('POST', "/concerts/{$concert->id}/orders", [
            'email' => 'john@example.com',
            'ticket_quantity' => 3,
-           'payment_token' => $paymentGateway->getValidTestToken()
+           'payment_token' => $this->paymentGateway->getValidTestToken()
         ]);
 
         // Assert
         $this->assertResponseStatus(201);
-        $this->assertEquals(9750, $paymentGateway->totalCharges());
+        $this->assertEquals(9750, $this->paymentGateway->totalCharges());
 
         $order = $concert->orders()->where('email', 'john@example.com')->first();
         $this->assertNotNull($order);
         $this->assertEquals(3, $order->tickets()->count());
+    }
 
+    /** @test */
+    function email_is_required_to_purchase_tickets()
+    {
+        $this->withExceptionHandling();
+        $concert = factory(Concert::class)->create();
+
+        $this->json('POST', "/concerts/{$concert->id}/orders", [
+            'ticket_quantity' => 3,
+            'payment_token'   => $this->paymentGateway->getValidTestToken()
+        ]);
+
+        $this->assertResponseStatus(422);
+        $this->assertArrayHasKey('email', array_get($this->decodeResponseJson(), 'errors'));
     }
 }
