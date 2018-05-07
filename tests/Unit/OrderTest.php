26e6cf87b20561b7159ae7f1dba46469dd9c679e
Connor Leech <connor@stitchlabs.com>
2018-05-07 15:46:34 -0700

    3.5 cancel failed orders

---
+++ tests/Unit/OrderTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: connor11528
+ * Date: 5/7/18
+ * Time: 3:35 PM
+ */
+
+namespace Tests\Unit;
+
+use App\Concert;
+use App\Order;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function tickets_are_released_when_an_order_is_cancelled()
+    {
+        $concert = factory(Concert::class)->create();
+        $concert->addTickets(10);
+        $order = $concert->orderTickets('jane@example.com', 5);
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+        $order->cancel();
+
+        $this->assertEquals(10, $concert->ticketsRemaining());
+        $this->assertNull(Order::find($order->id));
+    }
+}
