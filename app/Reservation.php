6cf7327032ba7785195a0dc8c4317705625ca008
Connor Leech <connor@stitchlabs.com>
2018-05-09 11:31:20 -0700

    add reservation complete method (51)

--- app/Reservation.php
+++ app/Reservation.php
@@ -36,4 +36,9 @@ class Reservation extends Model
     {
         return $this->email;
     }
+
+    public function complete()
+    {
+        return Order::forTickets($this->tickets(), $this->email(), $this->totalCost());
+    }
 }
--- tests/Unit/ReservationTest.php
+++ tests/Unit/ReservationTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\Concert;
 use App\Reservation;
+use App\Ticket;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Mockery;
 use Tests\TestCase;
@@ -30,13 +32,11 @@ class ReservationTest extends TestCase
     /** @test */
     function reserved_tickets_are_released_when_a_reservation_is_cancelled()
     {
-        $tickets     = collect(
-            [
-                Mockery::spy(Ticket::class),
-                Mockery::spy(Ticket::class),
-                Mockery::spy(Ticket::class),
-            ]
-        );
+        $tickets = collect([
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
+            Mockery::spy(Ticket::class),
+        ]);
         $reservation = new Reservation($tickets, 'jane@example.com');
         $reservation->cancel();
         foreach ($tickets as $ticket) {
@@ -54,5 +54,21 @@ class ReservationTest extends TestCase
         ]);
         $reservation = new Reservation($tickets, 'jane@example.com');
         $this->assertEquals($tickets, $reservation->tickets());
+    }
+
+    /** @test */
+    function completing_a_reservation()
+    {
+        $concert  = factory(Concert::class)->create(['ticket_price' => 1200]);
+        $tickets = factory(Ticket::class, 3)->create(['concert_id' => $concert->id]);
+        $reservation = new Reservation($tickets, 'john@example.com');
+
+        $order = $reservation->complete();
+
+        // check that calling reservation->complete creates an order
+        $this->assertEquals('john@example.com', $order->email);
+        $this->assertEquals(3, $order->ticketQuantity());
+        $this->assertEquals(3600, $order->amount);
+
     }
 }
